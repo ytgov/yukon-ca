3e9ef86a50fe3782eba3d2a69f565da066a2d26e
Franz Vanderlinde <franzvanderlinde@riseup.net>
2023-01-12 15:00:12 -0500

    refs #43250: DB configuration for Pantheon.

--- docroot/sites/default/settings.pantheon.php
+++ docroot/sites/default/settings.pantheon.php
@@ -198,3 +198,6 @@ if (empty($settings['file_scan_ignore_directories'])) {
     'bower_components',
   ];
 }
+
+$databases['migrate'] = $databases['default'];
+$databases['migrate']['default']['database'] = 'migrate';
