1bc7572ca85e61b0623a11de3f6dad5c3b12cb53
Dan Fitzsimmons <dan@prometsource.com>
2023-01-27 11:12:17 -0700

    refs #001: Add dependency injection for Department class.

--- docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/Department.php
+++ docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/Department.php
@@ -5,6 +5,7 @@ namespace Drupal\yukon_migrate\Plugin\migrate\process;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Adds taxonomy terms to node content.
@@ -26,6 +27,30 @@ use Drupal\migrate\Row;
 class Department extends ProcessPluginBase {
 
   /**
+   * EntityTypeManager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Construct.
+   *
+   * @param  array  $configuration
+   *    Configuration.
+   * @param $plugin_id
+   *    The plugin id.
+   * @param $plugin_definition
+   *    The plugin defintion.
+   * @param  \Drupal\Core\Entity\EntityTypeManagerInterface  $entityTypeManager
+   *    The entity type manager service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entityTypeManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
@@ -38,7 +63,7 @@ class Department extends ProcessPluginBase {
       $departmentData = [];
 
       foreach ($department as $tid) {
-        $departmentTerm = \Drupal::entityTypeManager()
+        $departmentTerm = $this->entityTypeManager
           ->getStorage('taxonomy_term')
           ->loadByProperties([
             'tid' => $tid,
