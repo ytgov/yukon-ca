9d7ecb89fd9453e9014765c2e8bf4d917d8d9e6b
Drupal CI <drupal-ci@ewdev.ca>
2023-02-15 15:07:03 -0500

    Automated commit by Drupal CI (master - 85dc05bcdac3dcfe1164a6bc75f9b2a10029fa26)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc9e80cd60ae6ccc6f019716d892a8665::getLoader();
+return ComposerAutoloaderInitdb70c34743817c1644eb1c947aabb3a0::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665
+class ComposerStaticInitdb70c34743817c1644eb1c947aabb3a0
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
@@ -400,8 +400,8 @@ class ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -6835,11 +6835,11 @@ class ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665::$prefixesPsr0;
-            $loader->fallbackDirsPsr0 = ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665::$fallbackDirsPsr0;
-            $loader->classMap = ComposerStaticInitc9e80cd60ae6ccc6f019716d892a8665::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitdb70c34743817c1644eb1c947aabb3a0::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitdb70c34743817c1644eb1c947aabb3a0::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitdb70c34743817c1644eb1c947aabb3a0::$prefixesPsr0;
+            $loader->fallbackDirsPsr0 = ComposerStaticInitdb70c34743817c1644eb1c947aabb3a0::$fallbackDirsPsr0;
+            $loader->classMap = ComposerStaticInitdb70c34743817c1644eb1c947aabb3a0::$classMap;
 
         }, null, ClassLoader::class);
     }
