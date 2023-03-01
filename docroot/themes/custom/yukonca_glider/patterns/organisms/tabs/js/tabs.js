8eb6ee2fed6e9679cfca257e3574b3d95289c84d
Viktoriia Sobutska <viktoriia.sobutska@evolvingweb.com>
2023-03-01 23:44:01 +0200

    refs #44850: Changed the structure and classes for pattern Tabs. JS code refactoring. Added general styles for node content h2 & h3.

--- docroot/themes/custom/yukonca_glider/patterns/organisms/tabs/js/tabs.js
+++ docroot/themes/custom/yukonca_glider/patterns/organisms/tabs/js/tabs.js
@@ -13,7 +13,7 @@
   Drupal.behaviors.tabsJS = {
     attach (context) {
       const tabContainer = $('.tabs', context);
-      const winLocation  = window.location;
+      const winLocation = window.location;
 
       tabContainer.each(() => {
         /* Navigate to tab if it exists in the URL. */
@@ -24,49 +24,34 @@
         $('a[data-toggle="tab"]', context).on('shown.bs.tab', (e) => {
           // On tab switch: update URL fragment.
           const hash = $(e.target).attr('href');
-          if (hash.substr(0, 1) === '#') {
-            winLocation.replace(`#!${hash.substr(1)}`);
-          }
           const tabScrollPosition = 'center';
           const $activeTab = document.querySelector(`${hash}-tab`);
           $activeTab.scrollIntoView({ behavior: 'smooth', block: tabScrollPosition });
         });
       });
+    },
+  };
 
-      $(once('arrows', '.pills-arrow-buttons .pills-btn-link', context)).click(function () {
-        const activeTab = $('.nav-pills a.nav-link.active');
-        const activeContent = $('.tab-content .tab-pane.show.active');
+  /**
+   * Script for the pills variant.
+   */
+  Drupal.behaviors.pillsJS = {
+    attach (context) {
+      $(once('arrows', '.pills-arrow .pills-btn-link', context)).click(function () {
         if ($(this).parent().hasClass('next-step')) {
-          $(this).parent().prev().children('.pills-btn-link').show();
-          activeTab.parent().next().children('a').addClass('active');
-          activeTab.removeClass('active');
-          activeContent.next().addClass('active');
-          activeContent.removeClass('active');
-
-          if ($('.nav-pills .nav-item:last-child a').hasClass('active')) {
-            $(this).hide();
-          }
+          $('.nav-pills .active').parent().next('li').find('a').tab('show');
         } else if ($(this).parent().hasClass('prev-step')) {
-          $(this).parent().next().children('.pills-btn-link').show();
-          activeTab.parent().prev().children('a').addClass('active');
-          activeTab.removeClass('active');
-
-          activeContent.prev().addClass('active');
-          activeContent.removeClass('active');
-          if ($('.nav-pills .nav-item:first-child a').hasClass('active')) {
-            $(this).hide();
-          }
+          $('.nav-pills .active').parent().prev('li').find('a').tab('show');
         }
       });
 
       $('.nav-pills .nav-item').click(function () {
-        $('.pills-arrow-buttons .btn-links .pills-btn-link').show();
-
+        $('.pills-arrow .btn-links').removeClass('hide');
         if ($('.nav-pills .nav-item:first-child a').hasClass('active')) {
-          $('.pills-arrow-buttons .prev-step.btn-links .pills-btn-link').hide();
+          $('.pills-arrow .prev-step.btn-links').addClass('hide');
         }
         if ($('.nav-pills .nav-item:last-child a').hasClass('active')) {
-          $('.pills-arrow-buttons .next-step.btn-links .pills-btn-link').hide();
+          $('.pills-arrow .next-step.btn-links').addClass('hide');
         }
       });
     },
