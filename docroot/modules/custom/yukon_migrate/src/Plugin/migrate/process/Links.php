31e7b64012d22d10535dfa32d9227ad500a7d8f1
Viktoriia Sobutska <viktoriia.sobutska@evolvingweb.com>
2023-03-09 11:28:48 +0200

    refs #45595: Changed the condition for uri.

--- docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/Links.php
+++ docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/Links.php
@@ -113,7 +113,7 @@ class Links extends MigrationLookup implements ContainerFactoryPluginInterface {
       $destination_id = parent::transform($source_nid, $migrate_executable, $row, $destination_property);
       if (!empty($destination_id)) {
         $destination_id = is_array($destination_id) ? reset($destination_id) : $destination_id;
-        $uri = "internal:/node/{$destination_id}";
+        $uri = is_string($value) ? "entity:node/{$destination_id}" : "internal:/node/{$destination_id}";
       }
     }
 
