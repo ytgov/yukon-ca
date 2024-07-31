c2920e0b0496079260629b71e176699f687f8ebb
Kirk Brown <kdborg@gmail.com>
2024-07-31 16:31:53 -0400

    refs #64845: Updating for code correctness.

--- docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/UriTransform.php
+++ docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/UriTransform.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\yukon_migrate\Plugin\migrate\process;
 
+use Drupal\Core\Database\Database;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
@@ -33,7 +34,6 @@ class UriTransform extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transformUri($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-
     $value = str_ireplace('"https://www.yukon.ca', '"', $value);
     $value = str_ireplace('"http://www.yukon.ca', '"', $value);
     $value = str_ireplace('"https://yukon.ca', '"', $value);
@@ -42,52 +42,52 @@ class UriTransform extends ProcessPluginBase {
     // [uuid-link:node:ba5dac36-a2a6-48df-a92c-dcba01cb40e5]
     $matches = [];
     while (preg_match('/\[uuid-link:node:([a-z0-9\-]+)\]/i', $value, $matches)) {
+      $uuid = $matches[1];
 
-        $uuid = $matches[1];
+      $types = [
+        'blog' => 'blog',
+        'campaign_page' => 'campaign_page',
+        'campground_directory_record' => 'campground_directory_record',
+        'department' => 'department',
+        'directory_records_places' => 'places',
+        'documents' => 'documents',
+        'engagement' => 'engagement',
+        'event' => 'event',
+        'in_page_alert' => 'in_page_alert',
+        'landing_page' => 'landing_page',
+        'landing_page_level_2' => 'landing_page_level_2',
+        'multi_step_page' => 'multi_step_page',
+        'news' => 'news',
+        'site_wide_alert' => 'site_wide_alert',
+        'topics_page' => 'topics_page',
+        'wetkit_page' => 'basic_page',
+        'contact' => 'contact',
+        'documents_non_branded' => 'documents',
+        'homepage' => 'homepage',
+        'webform' => 'contact',
+      ];
 
-        $types = [
-          'blog' => 'blog',
-          'campaign_page' => 'campaign_page',
-          'campground_directory_record' => 'campground_directory_record',
-          'department' => 'department',
-          'directory_records_places' => 'places',
-          'documents' => 'documents',
-          'engagement' => 'engagement',
-          'event' => 'event',
-          'in_page_alert' => 'in_page_alert',
-          'landing_page' => 'landing_page',
-          'landing_page_level_2' => 'landing_page_level_2',
-          'multi_step_page' => 'multi_step_page',
-          'news' => 'news',
-          'site_wide_alert' => 'site_wide_alert',
-          'topics_page' => 'topics_page',
-          'wetkit_page' => 'basic_page',
-          'contact' => 'contact',
-          'documents_non_branded' => 'documents',
-          'homepage' => 'homepage',
-          'webform' => 'contact',
-        ];
+      // SELECT  d.destid1 FROM db.migrate_map_yukon_migrate_basic_page d
+      // LEFT JOIN migrate.node m ON d.sourceid1 = m.nid WHERE
+      // m.uuid = '38d3c77f-cfd7-4dcf-8bce-c26de6eca988'.
+      $migrateDB = Database::getConnection('default', 'migrate');
+      $migrateQuery = $migrateDB->select('node', 'n');
+      $migrateQuery->fields('n', ['nid', 'type']);
+      $migrateQuery->condition('n.uuid', $uuid);
+      $migrateResult = $migrateQuery->execute()->fetchAssoc();
 
-        // SELECT  d.destid1 FROM db.migrate_map_yukon_migrate_basic_page d
-        // LEFT JOIN migrate.node m ON d.sourceid1 = m.nid WHERE m.uuid = '38d3c77f-cfd7-4dcf-8bce-c26de6eca988';
+      $query = \Drupal::database()
+        ->select('migrate_map_yukon_migrate_' . $types[$migrateResult['type']], 'm');
+      $query->fields('m', ['destid1']);
+      $migrateQuery->condition('m.sourceid1', $migrateResult['nid']);
+      $nid = $query->execute()->fetchField();
 
-        $migrateDB = \Drupal\Core\Database\Database::getConnection('default', 'migrate');
-        $migrateQuery = $migrateDB->select('node', 'n');
-        $migrateQuery->fields('n', ['nid', 'type']);
-        $migrateQuery->condition('n.uuid', $uuid);
-        $migrateResult = $migrateQuery->execute()->fetchAssoc();
-
-        $query = \Drupal::database()->select('migrate_map_yukon_migrate_' . $types[$migrateResult['type']], 'm');
-        $query->fields('m', ['destid1']);
-        $migrateQuery->condition('m.sourceid1', $migrateResult['nid']);
-        $nid = $query->execute()->fetchField();
-
-        if ($nid && $nid != 16158) {
-          $value = str_ireplace($matches[0], '/node/' . $nid, $value);
-        }
-        else {
-          $value = str_ireplace($matches[0], 'UUID_NOT_FOUND: ' . $uuid, $value);
-        }
+      if ($nid && $nid != 16158) {
+        $value = str_ireplace($matches[0], '/node/' . $nid, $value);
+      }
+      else {
+        $value = str_ireplace($matches[0], 'UUID_NOT_FOUND: ' . $uuid, $value);
+      }
     }
 
     return $value;
