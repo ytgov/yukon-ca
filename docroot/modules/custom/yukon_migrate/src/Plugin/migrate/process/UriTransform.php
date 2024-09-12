a54aff4ed2231fb65b1174103e3430eaf01b6702
W3 creative <contact@wecreatives.com>
2024-09-12 17:28:35 +0000

    Sep-12-2024

--- docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/UriTransform.php
+++ docroot/modules/custom/yukon_migrate/src/Plugin/migrate/process/UriTransform.php
@@ -196,6 +196,45 @@ final class UriTransform extends ProcessPluginBase {
       $value = str_ireplace($matches[0], '/node/'.$sourceNid, $value);
       $this->messenger()->addError($message);
     }
+    
+    while (preg_match('~\{"(?:[^{}]|(?R))*\}~', $value, $matches)) {
+        $data = json_decode($matches[0]);
+        
+
+        $db = Database::getConnection('default', 'migrate');
+        $migrateQuery = $db->select('file_managed', 'n');
+            $migrateQuery->fields('n', ['uri', 'filemime']);
+            $migrateQuery->condition('n.fid', $data->fid);
+        $result = $migrateQuery->execute()->fetchObject();
+        
+        $migrateQuery1 = $db->select('field_data_field_file_image_caption', 'n');
+            $migrateQuery1->fields('n', ['field_file_image_caption_value']);
+            $migrateQuery1->condition('n.entity_id', $data->fid);
+        $result1 = $migrateQuery1->execute()->fetchField();
+        
+        if ($result->filemime == "audio/mpeg") {
+            $url = \Drupal::service('file_url_generator')->generateAbsoluteString($result->uri);
+            $new_url = str_replace("http://yukonca.docksal.site", "", $url);
+            $image = '<div class="media media-element-container media-default">
+            <audio controls="controls" controlslist=""><source src="'.$new_url.'" type="audio/mpeg"></audio><span class="caption">'.$result1.'</span></div>';
+        }
+        else {
+           $url = \Drupal::service('file_url_generator')->generateAbsoluteString($result->uri);
+            $new_url = str_replace("http://yukonca.docksal.site", "", $url);
+            $style = '';
+            $alt = '';
+            if (isset($data->attributes->style)) {
+                $style = $data->attributes->style;
+            }
+            if (isset($data->attributes->alt)) {
+                $alt = $data->attributes->alt;
+            }
+            $image = "<div class='media media-element-container media-default'><img src='".$new_url."' class='".$data->attributes->class."' style='".$style."' alt='".$alt."'><span class='caption'>".$result1."</span></div>"; 
+        }
+        
+
+        $value = str_ireplace("[[".$matches[0]."]]", $image, $value);
+    }
 
     return $value;
   }
