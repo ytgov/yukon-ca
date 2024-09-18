b50510713e6640372bf7b1c4db24fa9a75ebbcd5
W3 creative <contact@wecreatives.com>
2024-09-18 22:24:38 +0000

    Sep-18-2024

---
+++ docroot/modules/custom/yukon_w3_custom/src/Plugin/Block/BlogFeedBlock.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\yukon_w3_custom\Plugin\Block;
+
+/**
+ * @file
+ * Contains \Drupal\matrics_data_upload\Plugin\Block\CertificateCheck.
+ */
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provide Committee I am Part of block.
+ *
+ * @Block(
+ *   id = "blog_feed_block",
+ *   admin_label = @Translation("Blog Feed Block"),
+ * )
+ */
+class BlogFeedBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * The language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new BookNavigationBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager service.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LanguageManagerInterface $language_manager, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->languageManager = $language_manager;
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new self($configuration, $plugin_id, $plugin_definition, $container->get('language_manager'), $container->get('current_route_match'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    return [
+      '#theme' => 'blog_feed',
+      '#language' => $language,
+      '#cache' => ['max-age' => 0],
+    ];
+  }
+
+}
