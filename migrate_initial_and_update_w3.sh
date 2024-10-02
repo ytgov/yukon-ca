df5c95dc3e80b57c48a1ffc8f183faf98cf93c82
W3 creative <contact@wecreatives.com>
2024-10-02 15:00:19 +0000

    Oct-2-2024-2

--- migrate_initial_and_update_w3.sh
+++ migrate_initial_and_update_w3.sh
@@ -47,3 +47,35 @@ echo "legacy_page_news..."
 $COMMAND migrate:import --group=legacy_page_news --update  --continue-on-failure
 echo "URL_alias..."
 $COMMAND migrate:import --group=legacy_url_alias --update  --continue-on-failure
+
+echo "reset_yukon_migrate_charts..."
+$COMMAND migrate:reset yukon_migrate_charts
+
+echo "legacy_user_role..."
+$COMMAND migrate:import --group=legacy_user_role --update  --continue-on-failure
+echo "legacy_taxonomies..."
+$COMMAND migrate:import --group=legacy_taxonomies --update  --continue-on-failure
+echo "legacy_media..."
+$COMMAND migrate:import --group=legacy_media --update  --continue-on-failure
+echo "legacy_paragraphs..."
+$COMMAND migrate:import --group=legacy_paragraphs --update  --continue-on-failure
+echo "legacy_documents..."
+$COMMAND migrate:import --group=legacy_documents --update  --continue-on-failure
+echo "legacy_menu..."
+$COMMAND migrate:import --group=legacy_menu --update  --continue-on-failure
+echo "legacy_files..."
+$COMMAND migrate:import --group=legacy_files --update  --continue-on-failure
+echo "legacy_nodes..."
+$COMMAND migrate:import --group=legacy_nodes --update  --continue-on-failure
+echo "legacy_basic_page..."
+$COMMAND migrate:import --group=legacy_basic_page --update  --continue-on-failure
+echo "legacy_page_news..."
+$COMMAND migrate:import --group=legacy_page_news --update  --continue-on-failure
+echo "URL_alias..."
+$COMMAND migrate:import --group=legacy_url_alias --update  --continue-on-failure
+
+
+
+# rerun this for fixed the sidebar
+echo "yukon_migrate_event..."
+$COMMAND migrate:import --update --continue-on-failure yukon_migrate_event
---
+++ migration_complete_w3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COMMAND="./vendor/bin/drush"
+
+echo "URL_alias..."
+$COMMAND migrate:import --group=legacy_url_alias  --continue-on-failure
