5025c4a59c701cdb1b2bd3631c4014cf466d17d5
Dave Rogers <dave.rogers@yukon.ca>
2025-05-13 15:53:55 -0700

    Add translation for jQuery Datatables chrome text.

--- docroot/modules/custom/yukon_hss_job_listings/js/datatable.js
+++ docroot/modules/custom/yukon_hss_job_listings/js/datatable.js
@@ -2,13 +2,26 @@
   Drupal.behaviors.yukon_hss_job_listings = {
     attach: function (context, settings) {
       if (context === document) {
-        currentLanguage = {
-          "emptyTable":     "The list of job postings is not available.",
-          "info":           "_START_ to _END_ of _TOTAL_ jobs",
-          "infoEmpty":      "",
-          "infoFiltered":   "(from _MAX_ total jobs)",
-          "loadingRecords": "",
-          "zeroRecords":    "No matching jobs were found."
+        if (window.location.href.indexOf("/fr/") > -1) {
+          currentLanguage = {
+            "emptyTable": "The list of job postings is not available. [Fr]",
+            "info": "_START_ to _END_ of _TOTAL_ jobs [Fr]",
+            "infoEmpty": "",
+            "infoFiltered": "(from _MAX_ total jobs) [Fr]",
+            "loadingRecords": "",
+            "search": "Search jobs [Fr]:",
+            "zeroRecords": "No matching jobs were found. [Fr]"
+          }
+        } else {
+          currentLanguage = {
+            "emptyTable": "The list of job postings is not available.",
+            "info": "_START_ to _END_ of _TOTAL_ jobs",
+            "infoEmpty": "",
+            "infoFiltered": "(from _MAX_ total jobs)",
+            "loadingRecords": "",
+            "search": "Search jobs:",
+            "zeroRecords": "No matching jobs were found."
+          }
         }
 
         new DataTable('#yukon-hss-hrsmart-job-listings', {
