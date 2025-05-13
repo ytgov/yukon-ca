28f7dac329e785f56dcd4920cbf52bff3cac5c90
Dave Rogers <dave.rogers@yukon.ca>
2025-05-13 14:12:47 -0700

    Use jQuery Datatables to inject pagination and filter for job listings.

---
+++ docroot/modules/custom/yukon_hss_job_listings/js/datatable.js
@@ -0,0 +1,30 @@
+(function ($, window, Drupal) {
+  Drupal.behaviors.yukon_hss_job_listings = {
+    attach: function (context, settings) {
+      if (context === document) {
+        currentLanguage = {
+          "emptyTable":     "The list of job postings is not available.",
+          "info":           "_START_ to _END_ of _TOTAL_ jobs",
+          "infoEmpty":      "",
+          "infoFiltered":   "(from _MAX_ total jobs)",
+          "loadingRecords": "",
+          "zeroRecords":    "No matching jobs were found."
+        }
+
+        new DataTable('#yukon-hss-hrsmart-job-listings', {
+          language: currentLanguage,
+          pageLength: 6,
+          lengthChange: false,
+          responsive: true,
+          columnDefs: [
+            // Highest priority: Job title
+            { responsivePriority: 1, targets: 2 },
+            // Second-highest priority: Req. #, Close date
+            { responsivePriority: 2, targets: 1 },
+            { responsivePriority: 2, targets: -1 }
+          ]
+        });
+      }
+    }
+  };
+})(jQuery, window, Drupal);
